d8ac74b6fa251a330254182856a2ded00a6f380f
TalysonSoares <talysonsoares40@gmail.com>
2022-10-09 19:05:40 -0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
-import Frases from './components/Frases';
+import SayMyName from './components/SayMyName';
 
-const url = `https://via.placeholder.com/150`
-const name = 'Talyson'
-const newName = name.toUpperCase()
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <h2>Alterando o JSX!</h2>
-      <p>Olá, {newName} </p>
-      <img src={url} alt="minha imagem" />
-      <Frases />
-      <HelloWorld />
-      <Frases />
-      <Frases />
+      <SayMyName nome="Talyson"/>
     </div>
   );
 }
