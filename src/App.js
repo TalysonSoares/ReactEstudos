c5c51ddf9b62b7b35c2eaf268a91debc930057f7
TalysonSoares <talysonsoares40@gmail.com>
2022-10-09 18:05:02 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Frases from './components/Frases';
+
+const url = `https://via.placeholder.com/150`
+const name = 'Talyson'
+const newName = name.toUpperCase()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá React!</h1>
+      <h2>Alterando o JSX!</h2>
+      <p>Olá, {newName} </p>
+      <img src={url} alt="minha imagem" />
+      <Frases />
+      <HelloWorld />
+      <Frases />
+      <Frases />
     </div>
   );
 }
