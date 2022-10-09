f55e1fd57366a774b234e95294406ef3152eed03
TalysonSoares <talysonsoares40@gmail.com>
2022-10-09 19:41:02 -0300

    Estudando componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-
+import HelloWorld from "./components/HelloWorld";
+import Frases from "./components/Frases"
 
 function App() {
+  const name = 'Talyson'
+  const newName = name.toUpperCase()
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <SayMyName nome="Talyson"/>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <img src={url} alt="Minha Imagem" />
+      <Frases />
+      <HelloWorld />
+      <Frases />
+      <Frases />
     </div>
   );
 }
